a245e7e2403b8225324efdaee422f83bba74993d
Neha Sinha <neha.sinha@zemosolabs.com>
2022-05-03 23:06:11 +0530

    Change promise getData

--- promises.js
+++ promises.js
@@ -1,12 +1,10 @@
-function getData(uId) {
-    fetch(uId)
-        .then(response=>{
-            console.log("Fetched the data!");
-            return "skc@gmail.com";
-        })
-        .then(email=>console.log("Email id of the user id is: " + email));
+function getData(uId,callback) {
+    setTimeout(() => {
+        console.log("Fetched the data!");
+        return callback("skc@gmail.com");
+        }, 4000);
 }  
 console.log("start");
-var email = getData("skc");
+var email = getData("skc",(email)=>{console.log("Email id of the user id is: " + email);});
 
 console.log("end");
